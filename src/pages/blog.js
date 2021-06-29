ff21e49b85a0b22fa974412c8e0bee659315f698
NaumanMunir9 <naumanmunir9@gmail.com>
2021-06-29 20:16:26 +0500

    images

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,73 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
+import { GatsbyImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const BlogPage = () => (
-  <Layout>
-    <Seo title="Blog Page" />
-    <h1>Hey There! This is the Blog Page</h1>
-    <p>Welcome to Blog Page</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const Blog = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allContentfulBlogPost(sort: { fields: date, order: DESC }) {
+          edges {
+            node {
+              title
+              id
+              slug
+              date(formatString: "Do MMMM, YYYY")
+              image {
+                fluid(maxWidth: 750) {
+                  ...GatsbyContentfulFluid
+                }
+              }
+              excerpt {
+                childMarkdownRemark {
+                  excerpt(pruneLength: 150)
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
 
-export default BlogPage
+  return (
+    <Layout>
+      <Seo title="Blog" />
+      <p>
+        <Link to="/">Go back to the homepage</Link>
+      </p>
+      <ul className="posts">
+        {data.allContentfulBlogPost.edges.map(edge => {
+          return (
+            <li className="post" key={edge.node.id}>
+              <h2>
+                <Link to={`/blog/${edge.node.slug}/`}>{edge.node.title}</Link>
+              </h2>
+              <div className="meta">
+                <span>Posted on {edge.node.date}</span>
+              </div>
+              {edge.node.image && (
+                <GatsbyImage
+                  className="featured"
+                  image={edge.node.image.fluid}
+                  alt={edge.node.title}
+                />
+              )}
+              <p className="excerpt">
+                {edge.node.excerpt.childMarkdownRemark.excerpt}
+              </p>
+              <div className="button">
+                <Link to={`/blog/${edge.node.slug}/`}>Read More</Link>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </Layout>
+  )
+}
+
+export default Blog
