5a48847973ebc9f07c1d1208d77c2d1c816ff4e2
NaumanMunir9 <naumanmunir9@gmail.com>
2021-06-29 17:32:01 +0500

    blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
+const BlogPage = () => (
   <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <Seo title="Blog Page" />
+    <h1>Hey There! This is the Blog Page</h1>
+    <p>Welcome to Blog Page</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default BlogPage
