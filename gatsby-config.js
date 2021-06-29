b87353971e67add29ee864bcea0ac78a7cec0733
NaumanMunir9 <naumanmunir9@gmail.com>
2021-06-29 17:35:55 +0500

    siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby-Contentful-Blog-Site`,
+    description: `Gatsby Contentful Blog Site`,
+    author: `@MNM`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>hey there!</h1>
+    <Seo title="Home Page" />
+    <h1>Hey There! This is the Home Page</h1>
     <Link to="/blog/">Visit the Blog Page</Link> <br />
   </Layout>
 )
